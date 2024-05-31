05f70a6716418a0035449ee86aecdc6e4a15c142
KKsaten <cocoar147@naver.com>
2024-05-31 10:26:39 +0900

    24.05.31_10:26 그외지정자 연습

---
+++ src/com/lsw/s4/S4Main.java
@@ -0,0 +1,25 @@
+package com.lsw.s4;
+
+public class S4Main {
+
+	public static void main(String[] args) {
+		
+		//클래스변수
+		//클래스명 변수명
+		Car.company = "";
+		System.out.println(Car.company);
+		
+		//클래스명.메서드명();
+		Car.info();
+		
+		System.out.println(Math.E);
+		
+		double b = Math.abs(1.2);
+		System.out.println(b);
+		
+//		Bird bird = new Bird();
+		//abstract 붙어서 객체 생성 안됨
+		
+	}
+	
+}
